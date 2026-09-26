4a877b895540a8ffe54a72a3bde1eadb2c3f1c38
precious1joe <162345921+precious1joe@users.noreply.github.com>
2026-09-26 13:44:20 +0100

    Fix PR #209: update batch.go

--- batch.go
+++ batch.go
@@ -401,7 +401,7 @@ func VerifyAll(
 					res.Address = addrStr
 				}
 
-				_, err = VerifyEntryContext(ctx, local, networkPassphrase)
+				_, err = VerifyEntry(local, networkPassphrase)
 				res.Error = err
 				mu.Lock()
 				results[j.index] = res
